241ada2c97693f0f0f27c167197ffa0e7c9ddd1d
Esraa-Sayed <53404081+Esraa-Sayed@users.noreply.github.com>
2021-05-23 16:33:06 +0200

    امتحان الوجهه الغاضب

--- app/src/main/java/com/example/log_in/AngryExam.java
+++ app/src/main/java/com/example/log_in/AngryExam.java
@@ -2,9 +2,13 @@ package com.example.log_in;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -12,53 +16,144 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class AngryExam extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener{
-    ImageView img1,img2,img3;
-    RadioGroup Q;
-    TextView TheQuastion;
-    RadioButton Q1Ch1,Q1Ch2,Q1Ch3;
+
+    RadioGroup Q1,Q2,Q3,Q4,Q5;
+    RadioButton Q1Answer,Q2Answer,Q3Answer,Q4Answer,Q5Answer;
     MediaPlayer Correct;
     MediaPlayer Wrong;
-    Runnable runnable = null;
-    Thread thread;
-    int countQ = 0,i=0;
+    int Score = 0 ;
+    int ExamQ =0;
+    Button score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.angry_exam);
-        img1 = findViewById(R.id.Img1);
-        img2 = findViewById(R.id.Img2);
-        img3 = findViewById(R.id.Img3);
-        TheQuastion = findViewById(R.id.TheQuastion);
-        Q = findViewById(R.id.Q);
-        Q1Ch1=findViewById(R.id.Q1Ch1);
-        Q1Ch2=findViewById(R.id.Q1Ch2);
-        Q1Ch3=findViewById(R.id.Q1Ch3);
-        createCard(i);
-        Q.setOnCheckedChangeListener(this);
+        Q1 = findViewById(R.id.Q1);
+        Q2 = findViewById(R.id.Q2);
+        Q3 = findViewById(R.id.Q3);
+        Q4 = findViewById(R.id.Q4);
+        Q5 = findViewById(R.id.Q5);
+        score = findViewById(R.id.Score);
+        Q1Answer = findViewById(R.id.Q1Answer);
+        Q2Answer = findViewById(R.id.Q2Answer);
+        Q3Answer = findViewById(R.id.Q3Answer);
+        Q4Answer = findViewById(R.id.Q4Answer);
+        Q5Answer = findViewById(R.id.Q5Answer);
+        Q1.setOnCheckedChangeListener(this);
+        Q2.setOnCheckedChangeListener(this);
+        Q3.setOnCheckedChangeListener(this);
+        Q4.setOnCheckedChangeListener(this);
+        Q5.setOnCheckedChangeListener(this);
         Correct = MediaPlayer.create(this,R.raw.rightanswer);
         Wrong =  MediaPlayer.create(this,R.raw.wronganswer);
-    }
-    private void createCard(int i) {
-        TheQuastion.setText("أختر الوجهه الغاضب");
-        img1.setImageResource( GlobalVar.Anger.get(i));
-        img2.setImageResource(GlobalVar.confusion.get(i));
-        img3.setImageResource(GlobalVar.confusion.get(i+1));
-    }
-    @Override
-    public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
-        //   Toast.makeText(this,checkedId+"",Toast.LENGTH_SHORT).show();
-        if(checkedId == Q1Ch1.getId()) {
-            Correct.start();
+        score.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(ExamQ < 5)
+                {
+                    Toast.makeText(AngryExam.this,"جاوب علي جميع الاسأله",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    View view2 = getLayoutInflater().inflate(R.layout.score_exam, null);
+                    TextView v = view2.findViewById(R.id.SocoreText);
+                    v.setText(Score + " من 5");
+                    AlertDialog.Builder builder;
+                    AlertDialog dialog;
+                    builder = new AlertDialog.Builder(AngryExam.this);
 
-           if(! Correct.isPlaying())
-           {
-               i++;
-               createCard(i);
-           }
+                    builder.setCancelable(false);
+                   builder.setView(view2);
+                    builder.setNegativeButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+
+                        }
+                    });
+                    dialog =builder.create();
+                    dialog.show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if(group==Q1)
+        {
+            ExamQ++;
+            Q1Answer.setText("الصحيحة");
+            if(checkedId == Q1Answer.getId()) {
+                Score++;
+                Correct.start();
+            }
+            else
+            {
+                Wrong.start();
+            }
+            for(int i = 0; i < Q1.getChildCount(); i++)
+                Q1.getChildAt(i).setEnabled(false);
         }
-        else {
-            Toast.makeText(this, "حاول مرة اخري", Toast.LENGTH_SHORT).show();
-            Wrong.start();
+        if(group==Q2)
+        {
+            ExamQ++;
+            Q2Answer.setText("الصحيحة");
+            if(checkedId == Q2Answer.getId()) {
+                Score++;
+                Correct.start();
+            }
+            else
+            {
+                Wrong.start();
+            }
+            for(int i = 0; i < Q2.getChildCount(); i++)
+                Q2.getChildAt(i).setEnabled(false);
+        }
+        if(group==Q3)
+        {
+            ExamQ++;
+            Q3Answer.setText("الصحيحة");
+            if(checkedId == Q3Answer.getId()) {
+                Score++;
+                Correct.start();
+            }
+            else
+            {
+                Wrong.start();
+            }
+            for(int i = 0; i < Q3.getChildCount(); i++)
+                Q3.getChildAt(i).setEnabled(false);
+        }
+        if(group==Q4)
+        {
+            ExamQ++;
+            Q4Answer.setText("الصحيحة");
+            if(checkedId == Q4Answer.getId()) {
+                Score++;
+                Correct.start();
+            }
+            else
+            {
+                Wrong.start();
+            }
+            for(int i = 0; i < Q4.getChildCount(); i++)
+                Q4.getChildAt(i).setEnabled(false);
+        }
+        if(group==Q5)
+        {
+            ExamQ++;
+            Q5Answer.setText("الصحيحة");
+            if(checkedId == Q5Answer.getId()) {
+                Score++;
+                Correct.start();
+            }
+            else
+            {
+                Wrong.start();
+            }
+            for(int i = 0; i < Q5.getChildCount(); i++)
+                Q5.getChildAt(i).setEnabled(false);
         }
 
     }
--- app/src/main/java/com/example/log_in/MainActivity.java
+++ app/src/main/java/com/example/log_in/MainActivity.java
@@ -49,7 +49,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(I);
             finish();
         }*/
-        GlobalVar Var = new GlobalVar();
         Intent I = new Intent(this, splashScrean.class);
         startActivity(I);
         finish();
